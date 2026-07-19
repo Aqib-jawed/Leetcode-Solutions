13378f3222986a0ba3b81a27680af28b46415621
Aqib Jawed <jawedaqib566@gmail.com>
2026-07-19 14:43:12 +0530

    Time: 2 ms (98.45%) | Memory: 42.8 MB (82.17%) - LeetSync

---
+++ 1159-smallest-subsequence-of-distinct-characters/smallest-subsequence-of-distinct-characters.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String smallestSubsequence(String s) {
+        int[] last = new int[26];
+
+        // Store last occurrence of each character
+        for (int i = 0; i < s.length(); i++) {
+            last[s.charAt(i) - 'a'] = i;
+        }
+
+        boolean[] used = new boolean[26];
+        StringBuilder stack = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            // Skip if already included
+            if (used[ch - 'a']) {
+                continue;
+            }
+
+            // Maintain lexicographically smallest order
+            while (stack.length() > 0 &&
+                   stack.charAt(stack.length() - 1) > ch &&
+                   last[stack.charAt(stack.length() - 1) - 'a'] > i) {
+
+                used[stack.charAt(stack.length() - 1) - 'a'] = false;
+                stack.deleteCharAt(stack.length() - 1);
+            }
+            stack.append(ch);
+            used[ch - 'a'] = true;
+        }
+        return stack.toString();
+    }
+}
